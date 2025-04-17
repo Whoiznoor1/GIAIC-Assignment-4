cdf6f00c23c2f10ff4c90bf808a62f91e6e6dff5
Noor un nisa Memon <163582696+Whoiznoor1@users.noreply.github.com>
2025-04-17 18:12:24 +0500

    Assignment4-00

---
+++ Assignment 00 to 07/00_intro_python/02_agreement_bot.py
@@ -0,0 +1,9 @@
+def main():
+    favorite_animal = input("\033[1;3m What's your favorite animal? \033[0m")
+
+    print(f"My Favorite animal is also {favorite_animal}!")
+
+
+if __name__ == "__main__":
+    main()
+
